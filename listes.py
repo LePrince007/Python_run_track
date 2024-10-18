57d70dedd3dad3b6ede8cd4712d3b5c0907f8206
Lamine Dxallo <dlamine.k@gmail.com>
2024-10-18 19:51:37 +0200

    les listes

---
+++ listes.py
@@ -0,0 +1,74 @@
+# *************EXERCICE 1**************
+"""
+def ma_liste():
+  fruits = ["pomme","cerise","orange"]
+  print(fruits)
+ma_liste()
+
+# *************EXERCICE 2**************
+def ma_liste():
+  fruits = ["pomme","cerise","orange"]
+  print(fruits[1])
+
+ma_liste()
+ 
+# *************EXERCICE 3**************
+def ma_liste():
+  fruits = ["pomme","cerise","orange"]
+  fruits.append("melon")
+  print(fruits)
+
+ma_liste()
+ 
+# *************EXERCICE 4**************
+
+def ma_liste():
+  fruits = ["pomme","cerise","orange","melon"]
+  fruits.insert(2, "mangue")
+  print(fruits)
+
+ma_liste()
+
+# *************EXERCICE 5**************
+L = [1,2,3,4,5]
+print(L)
+print(L[1])
+def add():
+  L[3] = L[2] + L[4]
+  print(L)
+add()
+print(L[4])
+
+# *************EXERCICE 6**************
+myList = [1,2,3,4,5]
+print(myList)
+a = myList[0]
+b = myList[4]
+myList[0] = b
+myList[4] = a
+print(myList)
+
+# *************EXERCICE 7**************
+
+la_liste = [8,24,48,2,16]
+count = 0
+for x in la_liste:
+  if x % 3 == 0:
+    count+= 1
+print(count)
+
+# *************EXERCICE 8**************
+
+L = [8, 24, 27, 48, 2,16, 9, 7, 84, 91]
+somme = 0
+for x in L:
+  if x % 2 == 0:
+    somme+= x
+
+print(somme)
+"""
+
+# *************EXERCICE 9**************
+
+
+  
