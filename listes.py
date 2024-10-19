ec7d318dcd0250dd050db04f037d70a91a5d3c18
Lamine Dxallo <dlamine.k@gmail.com>
2024-10-19 13:48:01 +0200

    liste_job

--- listes.py
+++ listes.py
@@ -66,9 +66,52 @@ for x in L:
     somme+= x
 
 print(somme)
-"""
+
 
 # *************EXERCICE 9**************
 
+L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
 
-  
+valeur1 = max(L)
+valeur2 = min(L)
+
+print(f"la valeur max est : {max(L)}")
+print(f"la valeur min est : {min(L)}")
+
+
+# *************EXERCICE 10**************
+
+L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91] 
+total = 1
+for i in L:
+  if i in range(25,90):
+    total = total * i
+print(total)
+
+
+# *************EXERCICE 11**************
+
+L = [7, 11, 42, 39, 2]
+liste = []
+for i in L:
+  liste.append(i+1)
+print(liste)
+
+
+# *************EXERCICE 12**************
+
+prime_numbers = [11, 3, 7, 5, 2]
+prime_numbers.sort()
+print(prime_numbers)
+"""
+
+# *************EXERCICE 13**************
+
+L = [10,20,30,20,10,50,60,40,80,50,40]
+
+
+
+
+# *************EXERCICE 14**************
+# *************EXERCICE 15**************
+
